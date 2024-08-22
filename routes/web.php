6b7813728e1ed067e31b837659b9a04d801531d7
web-intelligent <wwi19878@mail.ru>
2024-08-22 16:26:54 +0600

    Попытка 2

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,13 @@ Route::post('/email/verification-notification', function (Request $request) {
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
+Route::get('/test', function () {
+
+    dd('here');
+
+})->name('test');
+
+
 
 
 
